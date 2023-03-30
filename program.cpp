35199350cb3e2263de2dcedbd70d89486390d7f5
djordjeivanovic02 <djordjeivanovic05@hotmail.com>
2023-03-30 20:38:59 +0200

    C++ LinkedList

---
+++ program.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+struct Node{
+    int data;
+    Node* next;
+    Node* prev;
+};
+
+int main(){
+    cout << "Hello, World! How are you?" << endl;
+    return 1;
+}
